49b16a413022692b7eeb1f9ee627dc5d9d567465
Zoe Bisset <lemonadecode@gmail.com>
2026-06-14 22:45:53 -0400

    Update breakout.py

--- breakout.py
+++ breakout.py
@@ -9,3 +9,13 @@ PADDLE_Y = CANVAS_HEIGHT - 30
 PADDLE_WIDTH = 80
 PADDLE_HEIGHT = 15
 BALL_RADIUS = 10
+
+BRICK_GAP = 5
+BRICK_WIDTH = (CANVAS_WIDTH - BRICK_GAP*9) / 10
+BRICK_HEIGHT = 10
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+
+if _name_ == '__main__':
+    main()
