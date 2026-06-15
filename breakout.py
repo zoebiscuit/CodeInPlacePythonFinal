bfd7255eb8597247bbb01634596ae255d4aa5219
Zoe Bisset <lemonadecode@gmail.com>
2026-06-14 22:32:23 -0400

    setting up variables given by tutorial

---
+++ breakout.py
@@ -0,0 +1,11 @@
+from graphics import Canvas
+import time
+import random
+import math
+
+CANVAS_WIDTH = 500
+CANVAS_HEIGHT = 600
+PADDLE_Y = CANVAS_HEIGHT - 30 
+PADDLE_WIDTH = 80
+PADDLE_HEIGHT = 15
+BALL_RADIUS = 10
