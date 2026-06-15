92f3155725027e249fcb2e81f23cb9be3843bfb4
Zoe Bisset <lemonadecode@gmail.com>
2026-06-14 23:49:04 -0400

    holy cooked

--- breakout.py
+++ breakout.py
@@ -1,21 +1,66 @@
-from graphics import Canvas
-import time
 import random
-import math
 
-CANVAS_WIDTH = 500
-CANVAS_HEIGHT = 600
-PADDLE_Y = CANVAS_HEIGHT - 30 
-PADDLE_WIDTH = 80
-PADDLE_HEIGHT = 15
-BALL_RADIUS = 10
+LEXICON_FILE = "Lexicon.txt"
+INITIAL_GUESSES = 5
 
-BRICK_GAP = 5
-BRICK_WIDTH = (CANVAS_WIDTH - BRICK_GAP*9) / 10
-BRICK_HEIGHT = 10
+def play_game(secret_word):
+    pass
 
+def get_word():
+    index = random.randrange(3)
+    if index == 0:
+        return "HAPPY"
+    elif index == 1:
+        return "PYTHON"
+    else:
+        return "COMPUTER"
+    
 def main():
-    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    count = 0
+    secret_word = get_word().lower()
+    play_game(secret_word)
 
-if _name_ == '__main__':
+    revealed_word = ""
+    for i in range(len(secret_word)):
+        revealed_word += "-"
+
+    print("Welcome to a simple game of Wordle. All done in Python by Zoe Bisset!")
+    print(f"The word has {len(secret_word)} letters.")
+
+    for i in range(INITIAL_GUESSES):
+        print(f"The word looks like this {revealed_word}")
+        print(f"You are on guess number {INITIAL_GUESSES-count}")
+        guess = input("Type a single letter here, then press enter: ")
+        guess = guess.lower()
+        count += 1
+        x = 0
+
+        while x<len(revealed_word):
+            position = secret_word.find(guess)
+            if int(position) == -1:
+                print("That letter is not found!")
+                x = len(revealed_word) + 1
+            else:
+                print(f"Letter number {position+1} is {guess}")
+                secret_word = secret_word.replace(guess, "-")
+                revealed_word = revealed_word.strip(0, position) + guess + revealed_word.slice(position, len(revealed_word))
+                print(revealed_word)
+
+
+
+            
+        
+            if revealed_word == secret_word:
+                print(f"WORDLE! You got it in {INITIAL_GUESSES-count} guesses! The word was {secret_word}")
+#######################################################################
+
+
+
+    if revealed_word == secret_word:
+        print(f"WORDLE! You got it in {INITIAL_GUESSES-count} guesses! The word was {secret_word}")
+    else:
+        print(f"Aww man! You ran out of guesses! The word was {secret_word}")
+
+
+if __name__ == '__main__':
     main()
